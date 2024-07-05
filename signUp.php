d9dea2561e4e26efb4c1748205ca633906d263af
Kenta12340 <86399751+Kenta12340@users.noreply.github.com>
2024-07-05 11:23:40 +0900

    サインアップ画面作成

---
+++ signUp.php
@@ -0,0 +1,28 @@
+<head>
+    <link  rel="stylesheet" type="text/css" href="_css/signUp.css">
+</head>
+<body>
+    <main>
+            <div class="main-div_mainScreen">                
+              <div class="SingUp_main-div"><span>アカウント作成</span></div>
+                <form action="" method="post">
+                  <div class="SingUp_main-div_1-3">
+                      <label class="SignUp-main-div-label">　ユーザー名　</label>
+                      <input class="main-div-input" type="text" />
+                  </div>
+                  <div class="SingUp_main-div_1-3">
+                      <label class="SignUp-main-div-label">新規パスワード</label>
+                      <input class="main-div-input" type="password" />
+                  </div>
+                  <div class="SingUp_main-div_1-3">
+                      <label class="SignUp-main-div-label">パスワード確認</label>
+                      <input class="main-div-input" type="password" />
+                  </div>
+                  <div class="main-div_SigUpBtn">
+                      <input type="submit" class="SigUp-main-div-div_Btn" value="確認" />
+                  </div> 
+                </form>
+            </div>
+
+        </main>
+</body>
