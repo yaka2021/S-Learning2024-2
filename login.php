baa55fd87332443a0ad2c5a6b05fec0613f9813c
Kenta12340 <86399751+Kenta12340@users.noreply.github.com>
2024-07-05 10:27:18 +0900

    ログイン画面作成

---
+++ login.php
@@ -0,0 +1,23 @@
+<head>
+<link  rel="stylesheet" type="text/css" href="_css/login.css">
+</head>
+<body>
+<main>
+            <div class="main-div_mainScreen">                
+                    <div class="SignIn_main-div"><span>S-learning 2023</span></div>
+                <div class="SignIn_main-div_1-3">
+                    <label class="SignIn-main-div-label">ユーザー名</label>
+                    <input class="main-div-input" type="text" />
+                </div>
+                <div class="SignIn_main-div_1-3">
+                    <label class="SignIn-main-div-label">パスワード</label>
+                    <input class="main-div-input" type="password" />
+                </div>
+                <div class="main-div_1-4">
+                    <div class="main-div-div_1-1"><span>ログイン</span></div>
+                    <div class="main-div-div_1-2">アカウントを持っていない方はこちら</div>
+                </div> 
+            </div>
+        </main>
+
+        </body>
