8bffb36bd2f137a846b07cedcd4a9ea8ef86b1b8
Kenta12340 <86399751+Kenta12340@users.noreply.github.com>
2024-07-05 11:21:15 +0900

    ログイン画面修正

--- login.php
+++ login.php
@@ -1,23 +1,24 @@
 <head>
-<link  rel="stylesheet" type="text/css" href="_css/login.css">
+  <link  rel="stylesheet" type="text/css" href="_css/login.css">
 </head>
 <body>
 <main>
             <div class="main-div_mainScreen">                
                     <div class="SignIn_main-div"><span>S-learning 2023</span></div>
-                <div class="SignIn_main-div_1-3">
-                    <label class="SignIn-main-div-label">ユーザー名</label>
-                    <input class="main-div-input" type="text" />
-                </div>
-                <div class="SignIn_main-div_1-3">
-                    <label class="SignIn-main-div-label">パスワード</label>
-                    <input class="main-div-input" type="password" />
-                </div>
-                <div class="main-div_1-4">
-                    <div class="main-div-div_1-1"><span>ログイン</span></div>
-                    <div class="main-div-div_1-2">アカウントを持っていない方はこちら</div>
-                </div> 
+                <form action="" method="post">
+                  <div class="SignIn_main-div_1-3">
+                      <label class="SignIn-main-div-label">ユーザー名</label>
+                      <input class="main-div-input" type="text" />
+                  </div>
+                  <div class="SignIn_main-div_1-3">
+                      <label class="SignIn-main-div-label">パスワード</label>
+                      <input class="main-div-input" type="password" />
+                  </div>
+                  <div class="main-div_1-4">
+                      <input type="submit" class="main-div-div_1-1" value="ログイン" />
+                      <a class="Btn_Atag" href=""><div class="main-div-div_1-2">アカウントを持っていない方はこちら</div></a>
+                  </div> 
+                </form>
             </div>
         </main>
-
-        </body>
+</body>
