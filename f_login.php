c9b1f390db609fc27e4e9572c7f25cbbb6944d2c
yaka2021 <cbky1211051@gn.iwasaki.ac.jp>
2024-07-17 13:51:10 +0900

    デザインを修正しました。

--- f_login.php
+++ f_login.php
@@ -1,30 +1,34 @@
-<html>
-<head>
-  <link  rel="stylesheet" type="text/css" href="_css/login.css">
-</head>
-<body>
-<main>
-            <div class="main-div_mainScreen">                
-                    <div class="SignIn_main-div"><span>S-learning 2024</span></div>
-                <form action="_background/b_login.php" method="post">
-                  <div class="SignIn_main-div_1-3">
-                      <label class="SignIn-main-div-label">ユーザー名</label>
-                      <input class="main-div-input" type="text"  name="username" minlength="1" maxlength="10" required/>
-                  </div>
-                  <div class="SignIn_main-div_1-3">
-                      <label class="SignIn-main-div-label">パスワード</label>
-                      <input class="main-div-input" type="password" name="password" required/>
-                  </div>
-                  <div class="main-div_1-4">
-                      <input type="submit" class="main-div-div_1-1" value="ログイン" />
-                      <a class="Btn_Atag" href="f_create_user.php"><div class="main-div-div_1-2">
-                        アカウントを持っていない方はこちら</div></a>
-                  </div> 
-                </form>
+<!DOCTYPE HTML>
+<html lang="ja">
+    <head>
+        <link  rel="stylesheet" type="text/css" href="_css/login.css">
+    </head>
+    <body>
+        <main>            
+            <div class="SignIn_main-div">
+            <img src="_topImg/favicon.ico" alt="icon" width="50" height="50">
+                <span>S-Learning 2024</span>
             </div>
+            <form action="_background/b_login.php" method="post">
+                <div class="SignIn_main-div_1-3">
+                    <label class="SignIn-main-div-label">ユーザー名</label>
+                    <input class="main-div-input" type="text"  name="username" minlength="1" maxlength="10" required/>
+                </div>
+                <div class="SignIn_main-div_1-3">
+                    <label class="SignIn-main-div-label">パスワード</label>
+                    <input class="main-div-input" type="password" name="password" required/>
+                </div>
+                <div class="main-div_1-4">
+                    <input type="submit" class="main-div-div_1-1" value="ログイン" />
+                    <a class="Btn_Atag" href="f_create_user.php">  
+                        <div class="main-div-div_1-2">アカウント作成はこちら</div>
+                    </a>
+                </div> 
+            </form>
         </main>
-</body>
+    </body>
 </html>
+
 <?php
     if (session_status() == PHP_SESSION_NONE) {
         session_start();
